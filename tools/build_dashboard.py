00d78bd881346180768bc818c768e1fb50e75614
nickchen494949 <chenminein2020@gmail.com>
2026-06-02 09:10:46 +0800

    fix: add anti-caching meta tags to prevent browser caching

--- tools/build_dashboard.py
+++ tools/build_dashboard.py
@@ -265,6 +265,9 @@ html = f"""<!DOCTYPE html>
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+<meta http-equiv="Pragma" content="no-cache">
+<meta http-equiv="Expires" content="0">
 <title>Webull Strategy Overview</title>
 <script src="https://cdn.plot.ly/plotly-2.32.0.min.js"></script>
 <style>
